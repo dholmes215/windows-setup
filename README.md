f5997962f147bbb7c08dd31ee08101e491817476
David Holmes <dholmes@dholmes.us>
2021-06-21 14:16:52 -0700

    Initial implementation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Windows application setup script
+
+This is a PowerShell script to run on a new Windows install to configure it for software development and install a bunch of software development tools.  Uses Chocolatey for most packages.
+
+It hardcodes my name and email address, so you probably shouldn't run it if you're not me.
---
+++ upgrade.ps1
@@ -0,0 +1,19 @@
+$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $key Hidden 1
+Set-ItemProperty $key HideFileExt 0
+Set-ItemProperty $key ShowSuperHidden 1
+Stop-Process -processname explorer
+
+choco upgrade -y cmake
+choco upgrade -y conan
+choco upgrade -y firefox
+choco upgrade -y git
+choco upgrade -y visualstudio2019community
+choco upgrade -y visualstudio2019-workload-nativedesktop
+choco upgrade -y vscode
+choco upgrade -y vscode-cpptools
+choco upgrade -y vscode-powershell
+choco upgrade -y wsl2
+
+git config --global user.email "dholmes@dholmes.us"
+git config --global user.name "David Holmes"
